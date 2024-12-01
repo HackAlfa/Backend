b8f2ec812cb9f8f256ac73a64fa66f5748af9661
Nikita Kozel <niknikkzel@gmail.com>
2024-12-01 14:01:01 +0300

    add cors policy

--- server/handlers.go
+++ server/handlers.go
@@ -93,6 +93,7 @@ func recomendation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(recomendation))
