a984b7452733bc693659bdd011f4cc1efe246a1c
Nikita Kozel <niknikkzel@gmail.com>
2024-12-01 14:17:24 +0300

    add headers

--- server/handlers.go
+++ server/handlers.go
@@ -94,6 +94,8 @@ func recomendation(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(recomendation))
